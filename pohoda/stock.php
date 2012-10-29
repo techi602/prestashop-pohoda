18f9a99e04410f310053cac536d76a6fac989576
Michal Vrchota <michal.vrchota@gmail.com>
2012-10-29 01:13:31 +0100

    database

--- pohoda/stock.php
+++ pohoda/stock.php
@@ -56,24 +56,112 @@ $roots = $xpath->query('//rsp:responsePackItem/lStk:listStock');
 //var_dump($roots->length);
 //var_dump($roots);
 
-if($roots->length > 0){
-    for($i=0; $i < $roots->length; $i++){
+$shopId = 1; // administration
+
+$langId = 7; // detect ID with cs flag in database
+
+$db = DbCore::getInstance();
+
+
+$date = date('Y-m-d H:i:s');
+
+if ($roots->length > 0) {
+    for ($i = 0; $i < $roots->length; $i++) {
         $products = $xpath->query('./lStk:stock', $roots->item($i));
+        
         for ($j = 0; $j < $products->length; $j++) {
             $node = $products->item($j);
+    
+    		$name = $xpath->query('./stk:stockHeader/stk:name', $node)->item(0)->nodeValue;
+    		$shortName = $xpath->query('./stk:stockHeader/stk:shortName', $node)->item(0)->nodeValue;
+    		$code = @$xpath->query('./stk:stockHeader/stk:code', $node)->item(0)->nodeValue;
+    		$ean = @$xpath->query('./stk:stockHeader/stk:EAN', $node)->item(0)->nodeValue;
+    		$isSales = $xpath->query('./stk:stockHeader/stk:isSales', $node)->item(0)->nodeValue;
+    		$isInternet = $xpath->query('./stk:stockHeader/stk:isInternet', $node)->item(0)->nodeValue;
+    		$mass = $xpath->query('./stk:stockHeader/stk:mass', $node)->item(0)->nodeValue;
+    		$quantity = $xpath->query('./stk:stockHeader/stk:count', $node)->item(0)->nodeValue;
+    		$sellingPrice = $xpath->query('./stk:stockHeader/stk:sellingPrice', $node)->item(0)->nodeValue;
+    		$producer = $xpath->query('./stk:stockHeader/stk:producer', $node)->item(0)->nodeValue;
+    		$availability = $xpath->query('./stk:stockHeader/stk:availability', $node)->item(0)->nodeValue;
+    		$description = $xpath->query('./stk:stockHeader/stk:description', $node)->item(0)->nodeValue;
+    		$description2 = $xpath->query('./stk:stockHeader/stk:description2', $node)->item(0)->nodeValue;
 
-		$name = $xpath->query('./stk:stockHeader/stk:name', $node)->item(0)->nodeValue;
-		$shortName = $xpath->query('./stk:stockHeader/stk:shortName', $node)->item(0)->nodeValue;
-		$code = @$xpath->query('./stk:stockHeader/stk:code', $node)->item(0)->nodeValue;
-		$ean = @$xpath->query('./stk:stockHeader/stk:EAN', $node)->item(0)->nodeValue;
-		$isSales = $xpath->query('./stk:stockHeader/stk:isSales', $node)->item(0)->nodeValue;
-		$isInternet = $xpath->query('./stk:stockHeader/stk:isInternet', $node)->item(0)->nodeValue;
-		$mass = $xpath->query('./stk:stockHeader/stk:mass', $node)->item(0)->nodeValue;
-		$quantity = $xpath->query('./stk:stockHeader/stk:count', $node)->item(0)->nodeValue;
-		$sellingPrice = $xpath->query('./stk:stockHeader/stk:sellingPrice', $node)->item(0)->nodeValue;
-		$producer = $xpath->query('./stk:stockHeader/stk:producer', $node)->item(0)->nodeValue;
-		$availability = $xpath->query('./stk:stockHeader/stk:availability', $node)->item(0)->nodeValue;
+    		
+    		$query = new DbQuery();
+    		$query->select(implode(', ', array('p.`id_product`', 'p.`upc`', 'p.`ean13`')));
+    		$query->from('product', 'p');
+    		$query->where("p.ean13 = '" . $db->escape($ean) . "'");
+    		
+    		
+    		
+    		$result = $db->getRow($query);
+    		
+    		
+    		$data = array();
+    		$data['ean13'] = $ean;
+    		$data['upc'] = $code;
+    		$data['quantity'] = $quantity;
+    		$data['weight'] = $mass;
+    		$data['active'] = $isSales;
+    		$data['price'] = $sellingPrice;
+    		
+    		$data['date_upd'] = $date;
+    		
+    		$langdata = array();
+    		$langdata['id_product'] = $id;
+    		$langdata['id_shop'] = $shopId;
+    		$langdata['id_lang'] = $langId;
+    		$langdata['description'] = $description2;
+    		$langdata['description_short'] = $description;
+    		$langdata['link_rewrite'] = strtolower($name);
+    		$langdata['name'] = $name;
+    		$langdata['available_now'] = 'Skladem';
+    		$langdata['available_later'] = $availability;
+    		
+    		$shopdata = array();
+    		
+    		$langdata['id_shop'] = $shopId;
+    		$langdata['date_upd'] = $date;
+    		
+    		
+    		if (!$result) {
+    		    
+    		    $data['date_add'] = $date;
+    		    
+    		    
 
+    		    $db->insert('product', $data);
+    		    
+    		    $id = $db->Insert_ID();
+    		    
+    		    
+    		    $langdata['date_add'] = $date;
+    		    $langdata['id_product'] = $id;
+    		    $db->insert('product_lang', $langdata);
+    		    
+    		    
+    		    echo "Inserting product $id $ean<br>\n";
+    		    
+    		   
+    		    
+    		    
+    		} else {
+    		    
+    		    $db->insert('product', $data, 'product_id = ' . (int) $result['id_product']);
+    		    
+    		    echo "Updating product $id $ean<br>\n";
+    		    
+    		    $langdata['product_id'] = $result['id_product'];
+    		    
+    		    
+    		    $db->insert('product_lang', $langdata, false, false, Db::REPLACE);
+    		    
+    		    
+    		}
+    		
+    		
+    		
+    		
 
 /*
 echo $name ."\n";
