f0cf416f0e9fca6375182f772e5fb8139617c6fa
Michal Vrchota <michal.vrchota@gmail.com>
2012-11-27 23:25:31 +0100

    Functions moved to separate file

--- pohoda/stock.php
+++ pohoda/stock.php
@@ -27,6 +27,7 @@
 
 include(dirname(__FILE__).'/../../config/config.inc.php');
 require_once(dirname(__FILE__).'/../../init.php');
+require_once(dirname(__FILE__).'functions.php');
 
 // Get data
 
@@ -43,30 +44,6 @@ $imageImportFolder = dirname(__FILE__) . '/../../import/images/';
 $headers = getallheaders();
 $debug = true;
 
-
-
-function createUrlSlug($url)
-{
-    $url = preg_replace('~[^\\pL0-9_]+~u', '-', $url);
-    $url = trim($url, "-");
-    $url = strtr(
-            $url,
-            array("\xc3\xa1"=>"a","\xc3\xa4"=>"a","\xc4\x8d"=>"c","\xc4\x8f"=>"d","\xc3\xa9"=>"e","\xc4\x9b"=>"e","\xc3\xad"=>"i","\xc4\xbe"=>"l","\xc4\xba"=>"l","\xc5\x88"=>"n","\xc3\xb3"=>"o","\xc3\xb6"=>"o","\xc5\x91"=>"o","\xc3\xb4"=>"o","\xc5\x99"=>"r","\xc5\x95"=>"r","\xc5\xa1"=>"s","\xc5\xa5"=>"t","\xc3\xba"=>"u","\xc5\xaf"=>"u","\xc3\xbc"=>"u","\xc5\xb1"=>"u","\xc3\xbd"=>"y","\xc5\xbe"=>"z","\xc3\x81"=>"A","\xc3\x84"=>"A","\xc4\x8c"=>"C","\xc4\x8e"=>"D","\xc3\x89"=>"E","\xc4\x9a"=>"E","\xc3\x8d"=>"I","\xc4\xbd"=>"L","\xc4\xb9"=>"L","\xc5\x87"=>"N","\xc3\x93"=>"O","\xc3\x96"=>"O","\xc5\x90"=>"O","\xc3\x94"=>"O","\xc5\x98"=>"R","\xc5\x94"=>"R","\xc5\xa0"=>"S","\xc5\xa4"=>"T","\xc3\x9a"=>"U","\xc5\xae"=>"U","\xc3\x9c"=>"U","\xc5\xb0"=>"U","\xc3\x9d"=>"Y","\xc5\xbd"=>"Z")
-    );
-    //$url = iconv("utf-8", "us-ascii//TRANSLIT", $url);
-    $url = strtolower($url);
-    $url = preg_replace('~[^-a-z0-9_]+~', '', $url);
-    return $url;
-}
-
-function addSlashesToArray(&$array)
-{
-    foreach ($array as $key => &$val) {
-        if (is_string($val)) {
-            $val = addslashes($val);
-        }
-    }
-}
 
 $log = array();
 function logResponse($message)
