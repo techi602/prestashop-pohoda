273d805da81908b0f267c2cf7151e72e788bb565
Michal Vrchota <michal.vrchota@gmail.com>
2012-12-01 22:57:41 +0100

    Blind support for parameters

---
+++ pohoda/parameters.php
@@ -0,0 +1,16 @@
+<?php
+$content = file_get_contents("php://input");
+$file = "parameters.xml";
+
+if (!empty($content)) {
+    $file = "parameters" . date("_Y-m-d_H-i-s") . ".xml";
+    file_put_contents($file, $content);
+}
+
+
+// Send feed
+header("Content-Type:text/xml; charset=utf-8");
+echo '<?xml version="1.0" encoding="UTF-8"?>'."\n";
+?>
+<rsp:responsePack xmlns:rsp="http://www.stormware.cz/schema/response.xsd" version="2.0" id="00000001" state="ok" application="Prestashop" note="Prestashop import">
+</rsp:responsePack>
