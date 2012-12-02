e66f70f0d2d09f3acd6c2c9609fc93c30ecd8220
Michal Vrchota <michal.vrchota@gmail.com>
2012-12-02 01:13:27 +0100

    Importing categories

--- pohoda/categories.php
+++ pohoda/categories.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Import list of categories from Pohoda
+ */
+
+include(dirname(__FILE__).'/../../config/config.inc.php');
+require_once(dirname(__FILE__).'/../../init.php');
+require_once(dirname(__FILE__).'/functions.php');
+
+
 $content = file_get_contents("php://input");
 $file = "categories.xml";
 
@@ -7,6 +16,103 @@ if (!empty($content)) {
     file_put_contents($file, $content);
 }
 
+function importCategories($file, $blindMode)
+{
+    
+    $xml = new XMLReader();
+    $xml->open($file);
+    
+    $depth = array();
+    
+    $parentId = null;
+    
+    $parents = array();
+    
+    $depth = 0;
+    
+    $db = DbCore::getInstance();
+    
+    while ($xml->read()) {
+        
+        if ($xml->nodeType == XmlReader::ELEMENT && $xml->name == 'ctg:name') {
+            $xml->read();
+        
+            $name = $xml->value;
+        }
+        
+        if ($xml->nodeType == XmlReader::ELEMENT && $xml->name == 'ctg:description') {
+            $xml->read();
+        
+            $description = $xml->value;
+        }
+        
+        if ($xml->nodeType == XmlReader::ELEMENT && $xml->name == 'ctg:sequence') {
+            $xml->read();
+        
+            $sequence = $xml->value;
+        }
+        
+        
+        
+        if ($xml->nodeType == XmlReader::ELEMENT && $xml->name == 'ctg:id') {
+            $xml->read();
+            
+            $id = (int) $xml->value;
+            
+            //echo "$id (" . @$parents[$depth] . ")<br>\n";
+        }
+
+        if ($xml->nodeType == XmlReader::ELEMENT && $xml->name == 'ctg:internetParams') {
+            // hack - we insert category here
+            $parent = 1;
+            if (isset($parents[$depth])) {
+                $parent = $parents[$depth];
+            }
+
+            if ($blindMode) {
+                try {
+                    // hack - we can not save category unless it already exists
+                    $db->insert('category', array('id_category' => $id, 'id_parent' => $parent, 'date_add' => date('Y-m-d H:i:s')));
+                } catch (Exception $e) {
+
+                }
+            } else {
+                try {
+                    $category = new Category();
+                    $category->id = $id;
+                    $category->id_category = $id;
+                    $category->name = $name;
+                    $category->link_rewrite = createUrlSlug($name);
+                    $category->description = $description;
+                    $category->position = $sequence;
+                    $category->id_parent = $parent;
+                    $category->save();
+                } catch (Exception $e) {
+                    echo $e->getMessage() . " $id $parent\n";
+                }
+            }
+        }
+
+        if ($xml->nodeType == XmlReader::ELEMENT && $xml->name == 'ctg:subCategories') {
+            $depth++;
+            
+            if (!isset($parents[$depth])) {
+                $parents[$depth] = $id;
+            } 
+        }
+        
+        if ($xml->nodeType == XmlReader::END_ELEMENT && $xml->name == 'ctg:subCategories') {
+            $depth--;
+        }
+    }
+    
+    $xml->close();
+}
+
+importCategories($file, true);
+importCategories($file, false);
+
+Category::regenerateEntireNtree();
 
 // Send feed
 header("Content-Type:text/xml; charset=utf-8");
